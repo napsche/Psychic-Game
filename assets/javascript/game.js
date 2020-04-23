178274506610e73fb064cde2284e06f745d99582
Nicole Apsche <napsche@kgtiger.com>
2020-04-23 15:59:46 -0400

    adding javascript to react to key push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,3 +11,16 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var randomLetter = Math.floor(Math.random() * alphabet.length);
 var computerChoice = alphabet[randomLetter];
 console.log(computerChoice)
+
+//make event for when key is pressed
+document.onkeyup = function(event) {
+    var userChoice = event.key; 
+    //userChoice must be letter A-Z
+    var input = /[a-z]/gi; 
+        if (!input.test(userChoice)) {
+            alert("Please choose a letter!");
+        }
+        else {
+            console.log(userChoice);
+        }
+}
