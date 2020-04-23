4c7deaffacf4f3bd5b7994f1167d669c21957628
Nicole Apsche <napsche@kgtiger.com>
2020-04-23 15:51:09 -0400

    adding javascript for computer guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
-//define variables
+//define variables for game
+var winCount = 0;
+var lossCount = 0;
+var guessRemain = 10; 
+var guessedLetters = [];
+
+//set up array for computer choices
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//have computer pick random letter
+var randomLetter = Math.floor(Math.random() * alphabet.length);
+var computerChoice = alphabet[randomLetter];
+console.log(computerChoice)
